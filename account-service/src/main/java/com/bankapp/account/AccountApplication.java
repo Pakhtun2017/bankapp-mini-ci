a27bf00798300cf23fd844c0025de1badf1f14f0
Ubuntu <ubuntu@ip-172-31-65-101.ec2.internal>
2026-02-02 14:52:42 +0000

    Update pom and both services

--- account-service/src/main/java/com/bankapp/account/AccountApplication.java
+++ account-service/src/main/java/com/bankapp/account/AccountApplication.java
@@ -1,21 +1,31 @@
 package com.bankapp.account;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
 public class AccountApplication {
 
     @Value("${ENABLE_AUDIT_LOGS:false}")
     private boolean auditEnabled;
 
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
     @GetMapping("/account")
     public String getAccount() {
-        if (auditEnabled) {
-            System.out.println("AUDIT: Account endpoint accessed");
-        }
-        return "Account balance: $1000";
+    if (auditEnabled) {
+        System.out.println("AUDIT ENABLED: /account endpoint accessed");
+    } else {
+        System.out.println("AUDIT DISABLED");
     }
+    return "Account balance: $1000";
+    }	
+    
 }
 
--- transaction-service/src/main/java/com/bankapp/transaction/TransactionApplication.java
+++ transaction-service/src/main/java/com/bankapp/transaction/TransactionApplication.java
@@ -1,19 +1,21 @@
 package com.bankapp.transaction;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
 public class TransactionApplication {
 
-    @Value("${DB_TIMEOUT:100}")
-    private int timeout;
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionApplication.class, args);
+    }
 
-    @GetMapping("/transactions")
-    public String getTransactions() throws InterruptedException {
-        Thread.sleep(timeout); // 👈 intentional problem
-        return "Transactions returned after " + timeout + "ms";
+    @GetMapping("/transaction")
+    public String getTransaction() {
+        return "Transaction OK";
     }
 }
 
